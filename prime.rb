d520bee2c844f24e5335b60d06dcfe3334f43a0e
mirthful-flag-5763 <irinagamb@gmail.com>
2019-04-22 18:00:53 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,18 @@
 # Add  code here!
-def prime?(num)
-  (2..num/2).none?{|i| n % i == 0}
+def prime? (n0
+ for d in 2..(n - 1)
+  if (n % d) == 0
+   return false
+  end
+ end
+
+ true
+end
+
+def is_prime2 n
+ foundDivider = false
+  for d in 2..(n - 1)
+   foundDivider = ((n % d) == 0) or foundDivider
+  end
+ not foundDivider
 end
