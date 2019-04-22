f7616432b94302f679f5581589e017553cd0a651
mirthful-flag-5763 <irinagamb@gmail.com>
2019-04-22 18:03:55 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,11 @@
 # Add  code here!
 def prime? (n)
- for d in 2..(n - 1)
-  if (n % d) == 0
-   return false
-  end
- end
-
- true
+  if n <= 1
+         false
+     elsif n == 2
+         true
+     else
+         (2..n/2).none? { |i| n % i == 0}
+     end
 
 end
